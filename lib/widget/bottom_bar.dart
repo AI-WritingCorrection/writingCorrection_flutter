8097b96c0f4dfb97515ec258c01c6f876c0a5cca
park sehwan <58384653+sehwan12@users.noreply.github.com>
2025-05-13 23:45:35 +0900

    Design/detailpage ui 수정

--- lib/widget/bottom_bar.dart
+++ lib/widget/bottom_bar.dart
@@ -5,7 +5,7 @@ class Bottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var newHeight=MediaQuery.of(context).size.height * 0.08;    
+    var newHeight = MediaQuery.of(context).size.height * 0.08;
     return Container(
       height: newHeight,
       color: const Color(0xFFCEEF91),
@@ -13,24 +13,52 @@ class Bottom extends StatelessWidget {
         height: 50,
         child: TabBar(
           labelColor: const Color.fromARGB(221, 241, 105, 14),
-            unselectedLabelColor: Colors.black54,
+          unselectedLabelColor: Colors.black54,
           indicatorColor: Colors.transparent,
           tabs: <Widget>[
             Tab(
-              icon: Icon(Icons.home, size: newHeight*0.41),
-              child: Text('홈', style: TextStyle(fontSize: newHeight*0.17, color: Colors.black87, fontWeight: FontWeight.bold)),
+              icon: Icon(Icons.home, size: newHeight * 0.41),
+              child: Text(
+                '홈',
+                style: TextStyle(
+                  fontSize: newHeight * 0.25,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
             Tab(
-                icon: Icon(Icons.school, size: newHeight*0.41),
-              child: Text('자유 공부', style: TextStyle(fontSize: newHeight*0.17, color: Colors.black87, fontWeight: FontWeight.bold)),
+              icon: Icon(Icons.school, size: newHeight * 0.41),
+              child: Text(
+                '자유 공부',
+                style: TextStyle(
+                  fontSize: newHeight * 0.25,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
             Tab(
-                icon: Icon(Icons.history, size: newHeight*0.41),
-              child: Text('기록', style: TextStyle(fontSize: newHeight*0.17, color: Colors.black87, fontWeight: FontWeight.bold)),
+              icon: Icon(Icons.history, size: newHeight * 0.41),
+              child: Text(
+                '기록',
+                style: TextStyle(
+                  fontSize: newHeight * 0.25,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
             Tab(
-                icon: Icon(Icons.person, size: newHeight*0.41),
-              child: Text('마이 페이지', style: TextStyle(fontSize: newHeight*0.17, color: Colors.black87, fontWeight: FontWeight.bold)),
+              icon: Icon(Icons.person, size: newHeight * 0.41),
+              child: Text(
+                '마이 페이지',
+                style: TextStyle(
+                  fontSize: newHeight * 0.25,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
