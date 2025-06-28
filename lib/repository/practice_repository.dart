541258f304bcc0cc1a63b3456aac0121f11fad58
park sehwan <58384653+sehwan12@users.noreply.github.com>
2025-06-28 21:59:10 +0900

    Remove/ 임시리포지토리 삭제

--- lib/repository/practice_repository.dart
+++
@@ -1,7 +0,0 @@
-// import '../model/practice.dart';
-
-// abstract class PracticeRepository {
-//   Future<List<Step>> getAllPractices();
-//   Future<Step> getPracticeByIndex(int id);
-//   int getPracticeNum();
-// }
